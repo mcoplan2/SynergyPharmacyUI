51cf00267beae87f7c96b1e9b5f2bf0c1fd06277
Matthew Coplan <mcoplanwork@gmail.com>
2022-07-17 12:02:31 -0400

    fixed displaying nested objects on datagrid. Added functionality for posting approving/denying requests, just need to figure out how to pass the object information back into axios correctly

--- src/pages/ApproveDenyRequestsPage.js
+++ src/pages/ApproveDenyRequestsPage.js
@@ -14,8 +14,9 @@ export default function ApproveDenyRequestsPage() {
     useEffect(() => {
         async function getAllRequests(){   
             try {
-                const res = await API.get("/requests")
+                const res = await API.get("/requests/type/OPEN")
                 setRequests(res.data)
+                console.log(res.data)
             } catch(error) {
                 console.log(error)
             }
@@ -23,39 +24,92 @@ export default function ApproveDenyRequestsPage() {
         getAllRequests();
         }, []);
 
-    
+        // TEST APPROVING WITH ONLY REQUEST ID?
+        // LINK BUTTONS VIA FUNCTION CALLING APU AND PASSING SELECTEDROWS.ID FOR APPROVE
+        // {nestedobject1: {nestedobject2: { name} } }?
+    console.log(selectedRows)
+    const onApproveSubmit = async (selectedRows) => {
+        try{
+            await API.post('requests/approve/1', {
+                dosageCount: 90,
+                dosageFreq: 23,
+                creator: {
+                    userId: 1
+                },
+                med: {
+                    id: 3
+                },
+                id: 12
+            })
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    const onDenySubmit = async ({selectedRows}) => {
+        try{
+            await API.post('requests/deny/11', {
+                body: JSON.stringify(selectedRows)
+            })
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
     const columns = [
-        { field: 'id', headerName: 'ID', width: 90 },
+        { field: 'id', headerName: 'ID', width: 60 },
         {
             field: 'firstName',
             headerName: 'First Name',
             width: 150,
+            valueGetter: (params) => {
+                return params.row.creator.firstName;
+            }
         },
         {
             field: 'lastName',
             headerName: 'Last Name',
             width: 150,
+            valueGetter: (params) => {
+                return params.row.creator.lastName;
+            }
+        },
+        {
+            field: 'name',
+            headerName: 'Medication',
+            width: 150,
+            valueGetter: (params) => {
+                return params.row.med.name;
+            }
+        },
+        {
+            field: 'stock',
+            headerName: 'Stock',
+            width: 110,
+            valueGetter: (params) => {
+                return params.row.med.stock;
+            }
         },
         {
             field: 'dosageCount',
             headerName: 'Dosage',
-            width: 150,
+            width: 90,
         },
         {
             field: 'dosageFreq',
-            headerName: 'Dosage Frequency',
-            width: 150,
+            headerName: 'Dosage Freq',
+            width: 110,
         },
         {
             field: 'requestType',
             headerName: 'Status',
-            type: 'number',
             width: 110,
         },
         ];
 
+    
   return (
-    <Box sx={{ height: 500, maxWidth: 800, minWidth:400, padding:15}}>
+    <Box sx={{ height: 500, maxWidth: 1000, minWidth:500, padding:15}}>
       <DataGrid
         sx={{backgroundColor:'black'}}
         rows={getRequests}
@@ -74,9 +128,9 @@ export default function ApproveDenyRequestsPage() {
           }}
       />
       <div>
-      <Button color="success" variant="outlined" onClick={()=> {console.log(selectedRows)}} sx={{backgroundColor:'black'}}>Approve</Button>
+      <Button color="success" variant="outlined" onClick={onApproveSubmit} sx={{backgroundColor:'black'}}>Approve</Button>
       </div>
-      <Button color="error" variant="outlined" onClick={()=> {console.log(selectedRows)}} sx={{backgroundColor:'black'}}>Deny</Button>
+      <Button color="error" variant="outlined" onClick={()=>console.log({selectedRows})} sx={{backgroundColor:'black'}}>Deny</Button>
     </Box>
   );
 }

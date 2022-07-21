aa5ad52bdc8754d8eff97eb2fe4a484b8e9c0714
Andrew Beauchemin <android@vt.edu>
2022-07-21 15:23:48 -0400

    User can now deny charges

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -5,29 +5,45 @@ import { useEffect, useState } from "react";
 import { updateApi, getUserById } from '../util/api';
 
 export default function Payment({payment, payable, appUser}){
-    const v = {
-        paymentId: payment.paymentId,
-        amount: payment.amount,
-        medicineId: {
-            id: payment.medicineId.id
-        },
-        payStatus: "FULLY_PAID",
-        userId:{
-            userId: payment.userId.userId
-        },
-        reqId:{
-            id: payment.reqId.id
-        },}
+    const [visiableFlashCard, setVisiableFlashCard] = useState(true);
 
     const payPayment = async () => {
+        setVisiableFlashCard(false)
+
         const { username, token } = appUser;
         const userId = await getUserById(username);
         try{
             const tokenAPI = updateApi(token);
+            tokenAPI.post('payments/update', {
+                paymentId: payment.paymentId,
+                amount: payment.amount,
+                medicineId: {
+                    id: payment.medicineId.id
+                },
+                payStatus: "FULLY_PAID",
+                userId:{
+                    userId: payment.userId.userId
+                },
+                reqId:{
+                    id: payment.reqId.id
+                },}
+            )
 
+        } catch(error) {
+            console.log(error)
+        }
+    }
 
-            console.log(v)
-            tokenAPI.post('payments/update', v
+    const deletePayment = async () => {
+        setVisiableFlashCard(false)
+
+        const { username, token } = appUser;
+        const userId = await getUserById(username);
+        try{
+            const tokenAPI = updateApi(token);
+            tokenAPI.delete('payments/update', {
+                paymentId: payment.paymentId,
+                }
             )
 
         } catch(error) {
@@ -41,42 +57,48 @@ export default function Payment({payment, payable, appUser}){
 
 
   return (
-    <Box 
-    sx={{
-        width: 450,
-        height: 235,
-        backgroundColor: 'black',
-        margin: 5,
-        border: '1px solid orange',
-        color: "#FFFFFF",
-        '&:hover': {
+    <>
+    {visiableFlashCard &&
+        <Box 
+        sx={{
+            width: 450,
+            height: 235,
             backgroundColor: 'black',
-            opacity: [0.9, 0.8, 0.95],
-            border: '1px solid white'
-        },
-    }}>
-        <Typography >
-            {payment.userId.username}
-        </Typography>
-        <hr color="orange"></hr>
-        <p></p>
-        <Typography >
-            {payment.medicineId.name} <br/>
-            ${payment.medicineId.price.toFixed(2)} per dosage <br/>
-            {payment.reqId.dosageCount} doses
-        </Typography>
-        <hr color="orange"></hr>
-        <p></p>
-        <Typography >
-            Amount: ${payment.amount.toFixed(2)}
-        </Typography>
+            margin: 5,
+            border: '1px solid orange',
+            color: "#FFFFFF",
+            '&:hover': {
+                backgroundColor: 'black',
+                opacity: [0.9, 0.8, 0.95],
+                border: '1px solid white'
+            },
+        }}>
+            <Typography >
+                {payment.userId.username}
+            </Typography>
+            <hr color="orange"></hr>
+            <p></p>
+            <Typography >
+                {payment.medicineId.name} <br/>
+                ${payment.medicineId.price.toFixed(2)} per dosage <br/>
+                {payment.reqId.dosageCount} doses
+            </Typography>
+            <hr color="orange"></hr>
+            <p></p>
+            <Typography >
+                Total: ${payment.amount.toFixed(2)}
+            </Typography>
 
-        {payable &&
-            <>
-                <br/>
-                <Button onClick={payPayment}>Submit</Button>
-            </>
-        }
-    </Box>
+            {payable &&
+                <>
+                    <br/>
+                    <Button onClick={payPayment}>Confirm</Button>
+                    <Button onClick={deletePayment}>Deny</Button>
+                </>
+            }
+        </Box>
+    }
+    </>
   )
+  
 }
